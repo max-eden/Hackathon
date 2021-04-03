7c3699230810710fab6ce8d49fe5f8ce3df93f3e
Sha Li <lisha.sh@hotmail.com>
2021-04-03 17:00:56 +0800

    landing_page is done

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
 // app.js
 App({
-  onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+  onLaunch: function() {
+    wx.BaaS = requirePlugin('sdkPlugin')
+    //让插件帮助完成登录、支付等功能
+    wx.BaaS.wxExtend(wx.login,
+     wx.getUserInfo,
+     wx.requestPayment)
+    let clientID = '07e80a10328631011d98'
+    wx.BaaS.init(clientID)
+    //wx.BaaS.init('07e80a10328631011d98')
+    //wx.BaaS.auth.loginWithWechat() // 静默登录
+    wx.BaaS.auth.loginWithWechat().then(user => {
+      wx.setStorageSync('userInfo', user);
+      console.log("current user login", user)
+      this.globalData.userInfo = user;
+    }, err => {
+      console.log("fail login")
+    }
+      )
+  },
+  
+  globalData: {
+    userInfo: wx.getStorageSync('userInfo'),
+  }
+
+})
 
     // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-  },
-  globalData: {
-    userInfo: null
-  }
-})
+
+
---
+++ pages/landing_page/landing_page.js
@@ -0,0 +1,41 @@
+// pages/landing_page/landing_page.js
+Page({
+
+
+  data: {
+    Image:[],
+    currentUser: null
+  },
+
+
+  onLoad: function (options) {
+    let Image = new wx.BaaS.File()
+    // Image.get('606825afaef83205fe78f759').then((res) => {
+    //   console.log(res)
+    //   this.setData({
+    //     Image: res.data
+    //   })
+    //   // success
+    // }, err => {
+    // // err
+    // })
+  },
+
+  userInfoHandler: function(data) {
+    const self = this
+    wx.BaaS.auth.loginWithWechat(data).then((res) => {
+      console.log('userInfo', res)
+      self.setData({
+        currentUser: res
+      })
+      wx.setStorageSync('userInfo', res)
+      getApp().globalData.userInfo = res
+    }, (err) => {
+
+    })
+    wx.navigateTo({
+      url: `/pages/menu-page/menu-page`,
+    });
+  }
+
+})
